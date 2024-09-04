7b8807d4dce8bc2e99e2908555e13a86b821b0a6
Heeeeeeeeeeen <159850954+HenryWSC@users.noreply.github.com>
2024-09-05 11:56:06 +1200

    rolling animation finally plays he just always rolls

--- first game/scripts/player.gd
+++ first game/scripts/player.gd
@@ -4,8 +4,6 @@ class_name is_falling
 
 var isRolling = false
 var isAttacking = false
-var current_animation = ""
-var animation_completed = false
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -345.0
@@ -14,72 +12,75 @@ const JUMP_VELOCITY = -345.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var sprite = $AnimatedSprite2D
 
-func _ready():
-	sprite.play("Idle")
-	sprite.connect("animation_finished", self, "_on_animation_finished")
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
 
 	# Get the input direction and handle the movement/deceleration.
+	# get direction of plauer (-1 left 0 unchanged 1 right
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#flip character
+	if direction >= 0:
+		sprite.flip_h = false
+	elif direction <= 0:
+		sprite.flip_h = true
 	
-	# Flip character
-	sprite.flip_h = direction < 0
-
-	# Manage animation states
+	if Input.is_action_just_pressed("roll"):
+		isRolling = true
+		
+	#changing animations
 	if is_on_floor():
-		if Input.is_action_just_pressed("roll"):
-			isRolling = true
-			isAttacking = false
-		elif Input.is_action_just_pressed("Attack 1") or Input.is_action_just_pressed("Attack 2") or Input.is_action_just_pressed("Attack 3"):
-			isAttacking = true
-			isRolling = false
+		if direction == 0:
+			pass
+		if isRolling == true:
+			sprite.play("roll")
+			
 		elif direction == 0:
-			isRolling = false
-			isAttacking = false
+			sprite.play("Idle")
+			isRolling == false
 		else:
-			isRolling = false
-			isAttacking = false
-
-		if isRolling:
-			set_animation("roll")
-		elif isAttacking:
-			set_animation("Attack")
-		elif direction == 0:
-			set_animation("Idle")
-		else:
-			set_animation("Run")
+			sprite.play("Run") 
+		isRolling == false
 	else:
-		if velocity.y > 0:
-			set_animation("Falling")
-		else:
-			set_animation("Jump")
-
-	# Apply movement
+		sprite.play("Jump")
+		isRolling == false
+	
+	if not is_on_floor() and velocity.y > 0:
+		sprite.play("Falling")
+		isRolling == false
+	
+	
+	
+	#Attacks
+	if is_on_floor():
+		if Input.is_action_just_pressed("Attack 1"):
+			sprite.play("Attack 1")
+			isRolling == false
+			
+		if Input.is_action_just_pressed("Attack 2"):
+			sprite.play("Attack 2")
+			isRolling == false
+			
+		if Input.is_action_just_pressed("Attack 3"):
+			sprite.play("Attack 3")
+			isRolling == false
+	
+	#apply movement
 	if direction:
 		velocity.x = direction * SPEED    
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-
-func set_animation(anim_name: String):
-	if current_animation != anim_name and not is_animation_playing():
-		print("Setting animation to: ", anim_name)
-		sprite.play(anim_name)
-		current_animation = anim_name
-		animation_completed = false
-
-func _on_animation_finished(anim_name: String):
-	animation_completed = true
