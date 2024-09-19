468a3d4174b3bca96828381fb6161aadb37ba857
jacksonsmortypants <160076803+jacksonsmortypants@users.noreply.github.com>
2024-09-19 12:30:14 +1200

    wall block move

--- first game/test.gd
+++ first game/test.gd
@@ -3,11 +3,11 @@ extends CharacterBody2D
 #lets him play animations
 @onready var sprite = $AnimatedSprite2D
 
-@export var speed : float = 200.0
-@export var change_direction_interval : float = 2.0
+@export var speed : float = 100.0
+@export var change_direction_interval : float = 1
 @export var move_duration : float = 1.0
 
-var direction : int = 1  # 1 for right, -1 for left
+var direction : int = 1
 var time_since_last_change : float = 0.0
 var time_moving : float = 0.0
 
@@ -41,13 +41,13 @@ func _process(delta: float) -> void:
 	else:
 		velocity = Vector2.ZERO
 
-	# Update the position of the CharacterBody2D
+	# Update the position
 	move_and_slide()
 
 func choose_random_direction() -> void:
-	# Randomly choose direction to move: left (-1) or right (1)
+	# Random direction
 	direction = randi_range(-1, 1)
-	if direction == 0:  # Ensure the direction is either -1 or 1
+	if direction == 0:  
 		direction = 1
 		
 #he will die if player touches him
@@ -60,9 +60,13 @@ func choose_random_direction() -> void:
 func _on_hitbox_body_entered(body):
 	if body.is_in_group("Playerz"):
 		sprite.play("die")
+		speed = 0
 		dead = true
+
 
 
 func _on_animated_sprite_2d_animation_finished():
 	
 		queue_free()
+
+
